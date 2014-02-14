7f72193483340f13d08c9826db9f0303984988cb
Julio Gori Corradi <jgcorradi@gmail.com>
2014-02-14 17:38:06 -0200

    Incluindo plugin de image-crop - Versao mais estavel e compativel com IE8

---
+++ imageCrop/scripts/main.js
@@ -0,0 +1,35 @@
+define(['jquery', 'imageCrop'], function ($) {
+    'use strict';
+
+    var Main = (function () {
+
+        function init () {
+            console.log('Your app will starts here.');
+
+            $('img').imageCrop({
+                  overlayOpacity : 0.5
+                // , overlayBgColor : 'orange'
+                , onSelect       : updateForm
+            });
+        }
+
+        var selectionExists;
+
+        function updateForm(crop) {
+            $('input#x').val(crop.x);
+            $('input#y').val(crop.y);
+            $('input#width').val(crop.width);
+            $('input#height').val(crop.height);
+            $('input#path').val(crop.path);
+            selectionExists = crop.selectionExists;
+        };
+
+        return {
+              'init'       : init
+            , 'updateForm' : updateForm
+        };
+
+    })();
+
+    return Main;
+});
